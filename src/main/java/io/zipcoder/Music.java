b3d5ab6c65eb303fc21fa52dee3aca77e1e9878a
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-10-19 15:03:27 -0400

    Initial commit

---
+++ src/main/java/io/zipcoder/Music.java
@@ -0,0 +1,14 @@
+package io.zipcoder;
+
+public class Music {
+
+    private String[] playList;
+
+    public Music(String[] playList){
+        this.playList = playList;
+    }
+
+    public Integer selection(Integer startIndex, String selection){
+        return null;
+    }
+}
