93f6b89450e0f2e452163d3ccd0087bb810a415a
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-16 23:34:25 -0500

    Music methods written, all tests pass

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,37 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Music {
 
     private String[] playList;
+    ArrayList<Integer> il = new ArrayList<Integer>();
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        for (int i=0; i < playList.length; i++) {
+            if (playList[i].equals(selection)) {
+                il.add(i);
+            }
+        }
+        int num = fastestRoute(startIndex, il);
+        return num;
+    }
+
+    public int fastestRoute(int index, ArrayList il) {
+        int lowest = 10;
+        for (Object i : il) {
+            if ((Integer) i - index < lowest && (Integer) i - index> 0) {
+                lowest = (Integer)i - index;
+            }
+            if ((index + playList.length) - (Integer) i < lowest && (index + playList.length) - (Integer) i > 0) {
+                lowest = (index + playList.length) - (Integer) i;
+            }
+        }
+        return lowest;
     }
 }
