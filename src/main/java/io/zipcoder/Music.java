a0d164158f5cd7947d9157611ec91c17f01db2fa
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-23 21:19:51 -0500

    refined method to be more robust, cover cases not in tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -25,11 +25,14 @@ public class Music {
     public int fastestRoute(int index, ArrayList il) {
         int lowest = 10;
         for (Object i : il) {
-            if ((Integer) i - index < lowest && (Integer) i - index> 0) {
+            if ( (Integer)i - index < lowest && (Integer) i - index> 0) {
                 lowest = (Integer)i - index;
             }
             if ((index + playList.length) - (Integer) i < lowest && (index + playList.length) - (Integer) i > 0) {
                 lowest = (index + playList.length) - (Integer) i;
+            }
+            if (((Integer) i + playList.length) - index < lowest) {
+                lowest = ((Integer) i + playList.length) - index;
             }
         }
         return lowest;
